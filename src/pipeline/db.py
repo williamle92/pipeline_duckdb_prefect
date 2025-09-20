9216c7cbecddbcebceac351edaa638620141811e
William Le <williamkle92@gmail.com>
2025-09-20 15:20:01 -0700

    feat: create async engine

---
+++ src/pipeline/db.py
@@ -0,0 +1,27 @@
+from typing import AsyncGenerator
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker, AsyncEngine
+from sqlalchemy.orm import DeclarativeBase
+
+from .configs import Configurations
+
+# Base class for all database models
+class Base(DeclarativeBase):
+    """Base class for all database models"""
+    pass
+
+
+# Create async engine using configuration
+async_engine: AsyncEngine = create_async_engine(Configurations.database.generate_db_url(), echo=True)
+
+# Create async session factory
+AsyncSessionLocal = async_sessionmaker(
+    async_engine, class_=AsyncSession, expire_on_commit=False
+)
+
+
+async def get_async_db() -> AsyncGenerator[AsyncSession, None]:
+    """Get asynchronous database session"""
+    async with AsyncSessionLocal() as session:
+        yield session
+
+
